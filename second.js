15dfc13ca53bc38ab9dec9dc6893123001c2a4d2
dinywka <dina_kul@mail.ru>
2021-01-22 15:10:29 +0600

    Version2

--- second.js
+++ second.js
@@ -2,40 +2,30 @@
    let computerScore = 0
    let playerScore = 0
    let computerArr = ["Rock", "Paper", "Scissors"]
+   let choises = document.querySelectorAll("img")
 
-   function playRound () {
-    let playerSelection = prompt("Please enter Rock, Paper or Scissors!")
+   function playRound (playerSelection) {
+    alert(playerSelection)
     let computerSelection = computerArr[Math.floor(Math.random() * computerArr.length)]
+  
   alert(computerSelection)
          if((computerSelection === "Paper" && playerSelection === "Rock") || (computerSelection === "Rock"
           && playerSelection === "Scissors") || (computerSelection === "Scissors" && playerSelection ===
           "Paper")) {
 alert("Computer wins the round!");
 computerScore++ 
+          } else if (computerSelection === playerSelection) {
+            alert("It is a tie!")
           }
           else {
 alert("You won the round!");
 playerScore++
           }
-          
-   }
+    }  
 
-
-
-playRound()
-alert(computerScore)
-alert(playerScore)
-
-playRound()
-alert(computerScore)
-alert(playerScore)
-
-playRound()
-alert(computerScore)
-alert(playerScore)
-
-playRound()
-alert(computerScore)
-alert(playerScore)
-
-
+    choises.forEach(image =>{
+      image.addEventListener('click', function(){
+        playRound(image.className)
+      })
+    })
+  
